3fa123d453e9314164efed28e736518305c89124
“yamilascofano” <yamilascofano@gmail.com>
2022-01-11 19:09:01 -0300

    creacion clase BecaForm

---
+++ src/main/java/com/adatp/model/BecaForm.java
@@ -0,0 +1,13 @@
+package com.adatp.model;
+
+import lombok.Data;
+
+@Data
+public class BecaForm {
+	private int id;
+	private boolean estudia;
+	private boolean trabaja;
+	private int ingresos;
+	private int familiaCargo;
+	private int idParticipante;
+}
