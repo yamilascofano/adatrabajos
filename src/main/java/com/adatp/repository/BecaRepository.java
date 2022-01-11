99015f48865e7165e893953af32c83d84e796d7b
“yamilascofano” <yamilascofano@gmail.com>
2022-01-11 20:37:10 -0300

    modificacion beca repository

--- src/main/java/com/adatp/repository/BecaRepository.java
+++ src/main/java/com/adatp/repository/BecaRepository.java
@@ -1,14 +1,10 @@
 package com.adatp.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
 
 import com.adatp.model.Beca;
-import com.adatp.model.Participante;
 
-public interface BecaRepository extends CrudRepository<Beca, Integer> {
-
-	Optional<Beca> findByParticipante(Participante participante);
+public interface BecaRepository extends CrudRepository<Beca, Integer>, QueryByExampleExecutor<Beca> {
 
 }
