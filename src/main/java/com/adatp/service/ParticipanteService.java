ab0ca2d66c22f3044b5ee9bac0ebf6bd56bf8cd1
“yamilascofano” <yamilascofano@gmail.com>
2022-01-20 17:08:21 -0300

    agrego rol de participante

--- src/main/java/com/adatp/service/ParticipanteService.java
+++ src/main/java/com/adatp/service/ParticipanteService.java
@@ -5,7 +5,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.adatp.Excepciones.NoAutorizadoException;
+import com.adatp.Excepciones.NoUsuarioException;
+import com.adatp.form.ParticipanteForm;
+import com.adatp.form.enums.Rol;
 import com.adatp.model.Participante;
+import com.adatp.model.Usuario;
 import com.adatp.repository.BecaRepository;
 import com.adatp.repository.ParticipanteRepository;
 
@@ -18,9 +23,25 @@ public class ParticipanteService {
 	BecaRepository becaRepository;
 	@Autowired
 	BecaService becaService;
+	@Autowired
+	UsuarioService usuarioService;
 
-	public Participante save(Participante participante) {
-		return participanteRepository.save(participante);
+	public Participante save(ParticipanteForm form) throws NoUsuarioException, NoAutorizadoException {
+		Optional<Usuario> us = usuarioService.findById(form.getIdUsuario());
+		if (us.isEmpty())
+			throw new NoUsuarioException("No existe usuario");
+		Usuario usuario = us.get();
+		if (!usuario.getRol().equals(Rol.PARTICIPANTE))
+			throw new NoAutorizadoException("Usuario no autorizado");
+		Participante par = new Participante();
+		par.setApellido(form.getApellido());
+		par.setNombre(form.getNombre());
+		par.setResidencia(form.getResidencia());
+		par.setGenero(form.getGenero());
+		par.setFechaNacimiento(form.getFechaNacimiento());
+		par.setTieneBeca(form.isTieneBeca());
+		par.setUsuario(usuario);
+		return participanteRepository.save(par);
 	}
 
 	public Iterable<Participante> findAll() {
