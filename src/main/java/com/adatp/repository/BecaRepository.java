6a8e1b632cb0c76f0fe097fab60fb378a5382b38
“yamilascofano” <yamilascofano@gmail.com>
2022-01-20 17:02:03 -0300

    agrego querybyexample

--- src/main/java/com/adatp/repository/BecaRepository.java
+++ src/main/java/com/adatp/repository/BecaRepository.java
@@ -1,10 +1,14 @@
 package com.adatp.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 
 import com.adatp.model.Beca;
+import com.adatp.model.Participante;
 
 public interface BecaRepository extends CrudRepository<Beca, Integer>, QueryByExampleExecutor<Beca> {
 
+	Optional<Beca> findByParticipante(Participante participante);
 }
