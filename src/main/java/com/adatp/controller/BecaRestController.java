02ec44bae6b81de3a3a30713681d1b0c0a0e4dfd
“yamilascofano” <yamilascofano@gmail.com>
2022-01-20 16:43:38 -0300

    agrego nuevas excepciones

--- src/main/java/com/adatp/controller/BecaRestController.java
+++ src/main/java/com/adatp/controller/BecaRestController.java
@@ -10,11 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.adatp.Excepciones.ParticipanteInvalidoException;
+import com.adatp.form.BecaForm;
 import com.adatp.model.Beca;
-import com.adatp.model.BecaForm;
 import com.adatp.service.BecaService;
 
 @RestController
@@ -23,28 +23,18 @@ public class BecaRestController {
 	@Autowired
 	BecaService becaService;
 
-	@GetMapping("/")
-	public Iterable<Beca> listadoBecas() {
-		return becaService.findAll();
-	}
-
-	@GetMapping("/")
-	public Optional<Beca> busquedaPorParticipante(@RequestParam int idParticipante) {
-		return becaService.findByParticipante(idParticipante);
-	}
-
 	@GetMapping("/{id}")
 	public Optional<Beca> listadoPorId(@PathVariable int id) {
 		return becaService.findById(id);
 	}
 
 	@PostMapping("/")
-	public Beca insertarBeca(@RequestBody BecaForm form) throws Exception {
+	public Beca insertarBeca(@RequestBody BecaForm form) throws ParticipanteInvalidoException {
 		return becaService.save(form);
 	}
 
 	@PutMapping("/")
-	public Beca modificarBeca(@RequestBody BecaForm form) throws Exception {
+	public Beca modificarBeca(@RequestBody BecaForm form) throws ParticipanteInvalidoException {
 		return becaService.save(form);
 	}
 
