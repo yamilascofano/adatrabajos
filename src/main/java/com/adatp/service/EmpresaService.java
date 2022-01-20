7f77921a5b6c36ddc747f387a101da509a6dec27
“yamilascofano” <yamilascofano@gmail.com>
2022-01-20 17:06:55 -0300

    agrego aprobacion de administrador

--- src/main/java/com/adatp/service/EmpresaService.java
+++ src/main/java/com/adatp/service/EmpresaService.java
@@ -5,7 +5,13 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.adatp.Excepciones.NoAutorizadoException;
+import com.adatp.Excepciones.NoUsuarioException;
+import com.adatp.form.EmpresaForm;
+import com.adatp.form.enums.Estado;
+import com.adatp.form.enums.Rol;
 import com.adatp.model.Empresa;
+import com.adatp.model.Usuario;
 import com.adatp.repository.EmpresaRepository;
 
 @Service
@@ -14,6 +20,8 @@ public class EmpresaService {
 	EmpresaRepository empresaRepository;
 	@Autowired
 	RepresentanteService representanteService;
+	@Autowired
+	UsuarioService usuarioService;
 
 	public Iterable<Empresa> findAll() {
 		return empresaRepository.findAll();
@@ -28,13 +36,32 @@ public class EmpresaService {
 	}
 
 	public Empresa save(Empresa empresa) {
+		empresa.setEstado(Estado.EN_ESPERA);
 		return empresaRepository.save(empresa);
 	}
 
-	// public Iterable<Empresa> findByRepresentante(int idRepresentante) {
-	// Representante representante = new Representante();
-	// representante.setId(idRepresentante);
-	// return empresaRepository.findByRepresentante(representante);
-	// }
+	public Empresa aprobar(EmpresaForm form) throws NoUsuarioException, NoAutorizadoException {
+		Optional<Usuario> us = usuarioService.findById(form.getIdUsuario());
+		if (us.isEmpty())
+			throw new NoUsuarioException("No existe usuario");
+		Usuario usuario = us.get();
+		if (!usuario.getRol().equals(Rol.ADMINISTRADOR))
+			throw new NoAutorizadoException("Usuario no autorizado");
+		Optional<Empresa> emp = empresaRepository.findById(form.getIdEmpresa());
+		Empresa e = emp.get();
+		Empresa empresa = new Empresa();
+		if (form.getEstado().equals(Estado.APROBADO))
+			empresa.setId(e.getId());
+		empresa.setCategoria(e.getCategoria());
+		empresa.setAñoFundacion(e.getAñoFundacion());
+		empresa.setCuil(e.getCuil());
+		empresa.setDireccion(e.getDireccion());
+		empresa.setNombre(e.getNombre());
+		empresa.setNumContacto(e.getNumContacto());
+		empresa.setTipoEmpresa(e.getTipoEmpresa());
+		empresa.setEstado(form.getEstado());
+		return empresaRepository.save(empresa);
+
+	}
 
 }
