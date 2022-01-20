e135e1e94dc417aff029dee47e515fa342df7546
“yamilascofano” <yamilascofano@gmail.com>
2022-01-20 17:04:52 -0300

    agrego excepciones propias

--- src/main/java/com/adatp/service/BecaService.java
+++ src/main/java/com/adatp/service/BecaService.java
@@ -3,11 +3,11 @@ package com.adatp.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
+import com.adatp.Excepciones.ParticipanteInvalidoException;
+import com.adatp.form.BecaForm;
 import com.adatp.model.Beca;
-import com.adatp.model.BecaForm;
 import com.adatp.model.Participante;
 import com.adatp.repository.BecaRepository;
 import com.adatp.repository.ParticipanteRepository;
@@ -21,44 +21,33 @@ public class BecaService {
 	@Autowired
 	ParticipanteService participanteService;
 
-	public Iterable<Beca> findAll() {
-		return becaRepository.findAll();
-	}
-
 	public Optional<Beca> findById(int id) {
 		return becaRepository.findById(id);
 	}
 
-	public Beca save(BecaForm form) throws Exception {
+	public Beca save(BecaForm form) throws ParticipanteInvalidoException {
 		Beca beca = new Beca();
 		Optional<Participante> par = participanteService.findById(form.getIdParticipante());
+		if (par.isEmpty())
+			throw new ParticipanteInvalidoException("No existe participante con ese id");
 		Participante participante = par.get();
-		if (!par.isPresent()) {
-			throw new Exception("No existe participante con ese id");
-		} else {
-			if (participante.isTieneBeca() == true) {
-				beca.setParticipante(participante);
-				beca.setEstudia(form.isEstudia());
-				beca.setFamiliaCargo(form.getFamiliaCargo());
-				beca.setIngresos(form.getIngresos());
-				beca.setTrabaja(form.isTrabaja());
-			}
-		}
+		if (participante.isTieneBeca() == true)
+			beca.setParticipante(participante);
+		beca.setEstudia(form.isEstudia());
+		beca.setFamiliaCargo(form.getFamiliaCargo());
+		beca.setIngresos(form.getIngresos());
+		beca.setTrabaja(form.isTrabaja());
 		return becaRepository.save(beca);
 	}
 
 	public void deleteById(int id) {
 		becaRepository.deleteById(id);
-
 	}
 
 	public Optional<Beca> findByParticipante(int idParticipante) {
 		Participante participante = new Participante();
 		participante.setId(idParticipante);
-		Beca beca = new Beca();
-		beca.setParticipante(participante);
-		Example<Beca> be = Example.of(beca);
-		return becaRepository.findOne(be);
+		return becaRepository.findByParticipante(participante);
 	}
 
 }
