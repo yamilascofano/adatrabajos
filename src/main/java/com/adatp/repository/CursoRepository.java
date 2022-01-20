3fa10b3c3d114373a625464b8c9f32e47692fc39
“yamilascofano” <yamilascofano@gmail.com>
2022-01-20 17:02:51 -0300

    agrego querybyexample

--- src/main/java/com/adatp/repository/CursoRepository.java
+++ src/main/java/com/adatp/repository/CursoRepository.java
@@ -1,12 +1,10 @@
 package com.adatp.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
 
 import com.adatp.model.Curso;
-import com.adatp.model.Empresa;
 
-public interface CursoRepository extends CrudRepository<Curso, Integer> {
-
-	Iterable<Curso> findByEmpresa(Empresa empresa);
+public interface CursoRepository extends CrudRepository<Curso, Integer>, QueryByExampleExecutor<Curso> {
 
 }
